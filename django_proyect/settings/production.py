984dee90445cc3a5b430aa909e4a96059130a3ef
iwasaez <iwasaez@gmail.com>
2025-12-15 12:43:35 -0300

    Fix: Add .onrender.com to ALLOWED_HOSTS for production

--- django_proyect/settings/production.py
+++ django_proyect/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_root')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
