0b1f6d8af792d2476464439393d3ecc3947393d2
iwasaez <iwasaez@gmail.com>
2025-12-15 13:51:21 -0300

    Feature: Prepared for PostgreSQL migration, added venv to gitignore

--- django_proyect/settings/production.py
+++ django_proyect/settings/production.py
@@ -1,9 +1,5 @@
 from .base import *
-
-DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
-
-SECRET_KEY = os.environ['SECRET_KEY']
-
+DEBUG = True
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_root')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
